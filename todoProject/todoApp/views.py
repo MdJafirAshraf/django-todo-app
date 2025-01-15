8f317bd1115c0da98149ac2f59774b2208506577
MdJafirAshraf <mdjafirashraf@gmail.com>
2025-01-15 23:11:08 +0530

    mark as done changes

--- todoProject/todoApp/views.py
+++ todoProject/todoApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponse
 from todoApp.models import Task
 
@@ -7,4 +7,11 @@ from todoApp.models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+
+def markAsDone(request, id):
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = True
+    task.save()
     return redirect('home')
